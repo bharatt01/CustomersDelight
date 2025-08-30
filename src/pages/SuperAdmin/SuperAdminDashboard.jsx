f14aaa6da4c163cf02294d59f3c73d8bc3adfeac
Your GitHub Username <bharatsharma2526@gmail.com>
2025-08-30 20:18:23 +0530

    deployment-test6

--- src/pages/SuperAdmin/SuperAdminDashboard.jsx
+++ src/pages/SuperAdmin/SuperAdminDashboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../../components/Navbar'
 import { auth } from "../../firebase";
 import AddStore from './AddStore'
 import ManageStores from './ManageStores'
